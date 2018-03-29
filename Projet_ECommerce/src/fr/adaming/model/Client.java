e70f79f1f8e9796f11125a862d573ca0d83bf24c
inti0327 <inti0327@DESKTOP-6FRA8CT>
2018-03-29 13:21:51 +0200

    cedric : classe client

--- Projet_ECommerce/src/fr/adaming/model/Client.java
+++ Projet_ECommerce/src/fr/adaming/model/Client.java
@@ -1,5 +1,91 @@
 package fr.adaming.model;
 
-public class Client {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="clients")
+public class Client  implements Serializable{
+
+	//déclaration des attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_cl")
+	private long idClient;
+	@Column(name="nom")
+	private  String nomClient;
+	private String adresse;
+	private String email;
+	private String tel;
+	
+	
+	//transformation association UML en Java :
+	//private List<Commande> commande;
+	
+	
+	//constructeurs :
+	public Client() {
+		super();
+	}
+	public Client(String nomClient, String adresse, String email, String tel) {
+		super();
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+	public Client(long idClient, String nomClient, String adresse, String email, String tel) {
+		super();
+		this.idClient = idClient;
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+	
+	
+	//get et set:
+	public long getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(long idClient) {
+		this.idClient = idClient;
+	}
+	public String getNomClient() {
+		return nomClient;
+	}
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+	
 }
