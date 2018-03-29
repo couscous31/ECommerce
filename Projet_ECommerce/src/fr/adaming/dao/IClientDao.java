e7af4f0af0d6755c58fb5089536143554c75d6ed
inti0350 <inti0350@DESKTOP-B588GH8>
2018-03-29 17:35:36 +0200

    Wyndy : AgentClient

---
+++ Projet_ECommerce/src/fr/adaming/dao/IClientDao.java
@@ -0,0 +1,14 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import fr.adaming.model.Client;
+
+@Local
+public interface IClientDao {
+	
+	public List<Client> getAllListClient(Client cl);
+
+}
---
+++ Projet_ECommerce/src/fr/adaming/managedbean/AgentManageBean.java
@@ -0,0 +1,67 @@
+package fr.adaming.managedbean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+
+import fr.adaming.model.Agent;
+import fr.adaming.model.Produit;
+import fr.adaming.service.IAgentService;
+
+public class AgentManageBean implements Serializable {
+
+	// Transformation UML en java
+	IAgentService agentService;
+
+	@EJB // Declarer l'agent comme attribut d'un managebean
+	private Agent agent;
+
+	// Constructeur
+	public AgentManageBean(Agent agent) {
+		this.agent = agent;
+	}
+
+	// G+S
+
+	public IAgentService getAgentService() {
+		return agentService;
+	}
+
+	public void setAgentService(IAgentService agentService) {
+		this.agentService = agentService;
+	}
+
+	public Agent getAgent() {
+		return agent;
+	}
+
+	public void setAgent(Agent agent) {
+		this.agent = agent;
+	}
+
+	// Methodes se connecter
+
+	public String seConnecter(){
+		
+			
+		Agent aOut=agentService.isExist(this.agent);
+		
+		if(aOut!=null){
+			
+
+	return "success";
+		
+	}
+	return"echec";
+}
+	
+	public String seDeconnecter(){
+		
+		//Fermer la session ouvert 
+				FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+			
+				return "login";
+	}
+}
