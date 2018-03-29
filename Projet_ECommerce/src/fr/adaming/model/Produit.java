e6231b4837c21705334d7f39e4235ecd16a7c9d6
inti0333 <inti0333@DESKTOP-B6HP6U1>
2018-03-29 13:16:43 +0200

    mathilde: Classes produits et panier

---
+++ Projet_ECommerce/src/fr/adaming/model/Produit.java
@@ -0,0 +1,103 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produits")
+
+public class Produit implements Serializable{
+	
+	//Attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_p")
+	private long id;
+	private String designation;
+	private String description;
+	private double prix;
+	private int quantite;
+	private boolean selectionne;
+	private byte[] photoProd;
+	
+
+	
+	//Constructeurs
+	public Produit() {
+		super();
+	}
+	public Produit(String designation, String description, double prix, int quantite, boolean selectionne,
+			byte[] photoProd) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photoProd = photoProd;
+	}
+	public Produit(long id, String designation, String description, double prix, int quantite, boolean selectionne,
+			byte[] photoProd) {
+		super();
+		this.id = id;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photoProd = photoProd;
+	}
+	
+	//Getter et setter
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+	public byte[] getPhotoProd() {
+		return photoProd;
+	}
+	public void setPhotoProd(byte[] photoProd) {
+		this.photoProd = photoProd;
+	}
+	
+	
+
+}
