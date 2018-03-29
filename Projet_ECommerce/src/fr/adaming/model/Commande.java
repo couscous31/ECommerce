773bbcd1acb80e6237bb69153a3a1ebd9d7f9ad4
inti0350 <inti0350@DESKTOP-B588GH8>
2018-03-29 14:49:33 +0200

    windy: commande

--- Projet_ECommerce/src/fr/adaming/model/Commande.java
+++ Projet_ECommerce/src/fr/adaming/model/Commande.java
@@ -1,1 +1,69 @@
+package fr.adaming.model;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="commandes")
+public class Commande  implements Serializable{
+	
+	//Declaration des attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_com")
+	private long idCommande;
+	private Date dateCommande;
+	
+	//association uml java :
+	@ManyToOne
+	@JoinColumn(name="cl_id", referencedColumnName="id_cl")
+	private Client client;
+	
+	//Constructeurs
+	public Commande() {
+		super();
+	}
+
+	public Commande(Date dateCommande) {
+		super();
+		this.dateCommande = dateCommande;
+	}
+
+	public Commande(long idCommande, Date dateCommande) {
+		super();
+		this.idCommande = idCommande;
+		this.dateCommande = dateCommande;
+	}
+
+	public long getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(long idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public Date getDateCommande() {
+		return dateCommande;
+	}
+
+	public void setDateCommande(Date dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+	
+	
+	
+	
+	
+	
+
+}
